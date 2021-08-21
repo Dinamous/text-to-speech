60a4efe1c46725492c71d98d141d053c926da02f
matheus simoes <matheusaugustosimoes@hotmail.com>
2021-08-21 20:54:11 -0300

    criação do front

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+    <h1>Dê voz a seus pensamentos</h1>
+
+    
+    <footer></footer>
+    </div>
+  );
+}
+
+export default App;
